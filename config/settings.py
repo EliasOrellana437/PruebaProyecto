360f0bed78df298b11a0df3cc030b09a20c5c684
EliasOrellana437 <eliasernestoorellanavasquez@gmail.com>
2025-11-27 16:50:12 -0600

    g

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'leccion_alfabeto',
     'core',  # nuestra app principal
 ]
 
