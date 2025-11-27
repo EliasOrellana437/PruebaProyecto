e6464198d138e58774d128a251ceedc82c2ab1a1
EliasOrellana437 <eliasernestoorellanavasquez@gmail.com>
2025-11-27 16:49:09 -0600

    asda

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leccion_alfabeto',
     'core',  # nuestra app principal
 ]
 
