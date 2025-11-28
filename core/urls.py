efa77325fc6f5880f0278e733e238ced31ef2123
EliasOrellana437 <eliasernestoorellanavasquez@gmail.com>
2025-11-27 21:26:17 -0600

    funciona porfavorrrrr

--- core/urls.py
+++ core/urls.py
@@ -23,5 +23,4 @@ path('mini-juego/', views.mini_juego, name='mini_juego'),
  # Números
 path("numeros/", views.numeros_view, name="numeros"),
 path("numeros/<int:numero>/", views.numeros_senal_view, name="numeros_senal"),
-
 ]
